2a38b666849cee73ab40ebd593520aa5f0bc5680
LightKazuto <ilhamsidi62@gmail.com>
2025-02-20 21:17:27 +0700

    update: add responsive for activeTab Postingan

--- app/src/pages/Dashboard/index.tsx
+++ app/src/pages/Dashboard/index.tsx
@@ -9,6 +9,32 @@ import {
   faAt,
 } from "@fortawesome/free-solid-svg-icons";
 
+interface Graduation {
+  id: number;
+  name: string;
+  major: string;
+  fromDate: string | number;
+  toDate: string | number;
+}
+
+interface Intro {
+  title: string;
+  address: {
+    icon: string;
+    text: string;
+  }[];
+  graduation: Graduation[];
+}
+
+interface ProfileData {
+  name: string;
+  date: string;
+  bio: {
+    text: string;
+  };
+  intro: Intro;
+}
+
 interface Photo {
   id: number;
   title: string;
@@ -18,23 +44,21 @@ interface Photo {
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState<string>("Postingan");
+  const [profile, setProfile] = useState<ProfileData | null>(null);
   const [photos, setPhotos] = useState<Photo[]>([]);
+
   const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
 
-  // Update to handle photo click and set the selected photo
-  const handleProfileClick = (photo: Photo | null) => {
-    setSelectedPhoto(photo); // Set selected photo in the state
-    setIsProfileOpen(true); // Open the modal
-  };
-
-  const closeProfileModal = () => {
-    setIsProfileOpen(false); // Close the modal
-  };
-
-  const handleTabClick = (tabName: string) => {
-    setActiveTab(tabName);
-  };
+  useEffect(() => {
+    fetch("/Tentang.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setProfile(data);
+        setPhotos(data.photos);
+      })
+      .catch((error) => console.error("Error fetching profile data:", error));
+  }, []);
 
   useEffect(() => {
     fetch("/photos.json")
@@ -45,125 +69,136 @@ const Dashboard = () => {
       .catch((error) => console.error("Error fetching photos:", error));
   }, []);
 
+  const handleTabClick = (tabName: string) => {
+    setActiveTab(tabName);
+  };
+
+  const handleProfileClick = (photo: Photo | null) => {
+    setSelectedPhoto(photo);
+    setIsProfileOpen(true);
+  };
+  const closeProfileModal = () => setIsProfileOpen(false);
+
   const renderTab = () => {
+    if (!profile) return <div>Loading...</div>;
+
+    const iconMap = {
+      faPhone: faPhone,
+      faAt: faAt,
+      faLocationDot: faLocationDot,
+    };
+
     switch (activeTab) {
       case "Postingan":
         return (
           <div className="w-full h-auto flex justify-center items-center mt-4">
-            <div className="flex flex-row-reverse grid-cols-2 w-[1300px] justify-between">
-              <div className="w-[750px]">
-                <div className="w-[750px] h-auto p-4 bg-white rounded-md shadow-md">
-                  <h3 className="text-lg font-bold font-segoe">Postingan</h3>
+            <div className="xl:flex flex flex-col-reverse xl:flex-row-reverse xl:grid-cols-2 w-[1300px] xl:justify-center justify-between">
+
+              <div className="xl:w-[750px] xl:mt-0 mt-4">
+                <div className="w-[750px] w-full h-auto p-4 bg-white rounded-md shadow-md "> 
+                  <h3 className="text-lg font-bold font-segoe xl:text-left text-center">Postingan</h3>
                 </div>
 
-                <div className="w-[750px] h-auto p-4 bg-white rounded-md shadow-md mt-4">
+                <div className="xl:w-[750px] h-auto p-4 bg-white rounded-md shadow-md mt-4 xl:items-center">
                   <div className="flex">
                     <img
                       src="https://profilepicture7.com/img/img_dongman/3/1382673276.jpg"
                       alt="Foto Profile"
                       className="w-12 rounded-full cursor-pointer"
-                      onClick={() => handleProfileClick(null)} // Placeholder for profile image click
+                      onClick={() =>
+                        handleProfileClick({
+                          id: 0,
+                          title: "Profile Image",
+                          url: "https://profilepicture7.com/img/img_dongman/3/1382673276.jpg",
+                          description: "Profile image",
+                        })
+                      }
                     />
+
                     <div className="flex flex-col ml-2">
                       <span className="text-black font-segoe font-semibold text-base">
-                        Ilham Sidiq
+                        {profile.name}
                       </span>
                       <span className="text-gray-500 font-segoe text-sm font-semibold">
-                        24 September 2024
+                        {profile.date}
                       </span>
                     </div>
                   </div>
                   <div>
                     <p className="text-black font-segoe mt-2 text-justify px-2">
-                      I am a result-oriented professional with over six years of
-                      experience in electronics and public affairs. With a
-                      degree in English Literature, I effectively combine my
-                      literary insights with technical skills in software
-                      engineering. I am proficient in various technologies,
-                      including React.js, Next.js, TypeScript, Python, and
-                      MySQL, which enable me to build responsive and efficient
-                      web applications. As an interim head at PT. Panasonic
-                      Gobel Indonesia, I achieved more than 80% of the
-                      improvement targets, demonstrating my ability to enhance
-                      team productivity. I believe my commitment to
-                      collaboration and excellence in communication are
-                      essential to achieving organizational goals. Ambitious and
-                      driven, I aim to develop impactful technology solutions as
-                      a Software Engineer in a progressive organization,
-                      leveraging my unique ability to approach challenges with
-                      both creativity and analytical precision.
+                      {profile.bio.text}
                     </p>
                   </div>
                 </div>
               </div>
 
-              <div className="flex flex-wrap">
-                <div className="w-[500px] h-[260px] p-4 bg-white rounded-md shadow-md">
-                  <h3 className="text-lg font-bold font-segoe">Intro</h3>
+              <div className="flex flex-wrap justify-evenly gap-4">
+                <div className="w-[500px] xl:h-[260px] p-4 bg-white rounded-md shadow-md ">
+                  <h3 className="text-lg font-bold font-segoe">
+                    {profile.intro.title}
+                  </h3>
                   <div className="text-gray-600 font-segoe mt-4 text-justify">
-                    <ul className="space-y-2">
-                      <li>
-                        <FontAwesomeIcon icon={faGraduationCap} />
-                        <span className="ml-2">
-                          Lulusan Fullstack Engineering di RevoU
-                        </span>
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faGraduationCap} />
-                        <span className="ml-2">
-                          Lulusan Sastra Inggris di Universitas Terbuka
-                        </span>
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faGraduationCap} />
-                        <span className="ml-2">
-                          Lulusan Teknik Komputer dan Jaringan di SMKN 2
-                          Baleendah
-                        </span>
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faPhone} />
-                        <span className="ml-2">+62822-9500-9833</span>
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faAt} />
-                        <span className="ml-2">ilhamsidiq62@gmail.com</span>
-                      </li>
-                      <li>
-                        <FontAwesomeIcon icon={faLocationDot} />
-                        <span className="ml-2">
-                          Kiaracondong Kota.Bandung Jawa Barat
-                        </span>
-                      </li>
+                    <ul className="mt-2 space-y-2">
+                      {profile.intro.graduation
+                        .sort((a, b) => b.id - a.id)
+                        .map((grad) => (
+                          <li
+                            key={grad.id}
+                            className="flex items-center text-gray-600">
+                            <FontAwesomeIcon
+                              icon={faGraduationCap}
+                              className="mr-2"
+                            />
+                            <div className="space-x-1">
+                              <span className="text-gray-500">
+                                {grad.major}
+                              </span>
+                              <span>di</span>
+                              <span>{grad.name}</span>
+                            </div>
+                          </li>
+                        ))}
+
+                      {/* Render address info */}
+                      {profile.intro.address.map((item, index) => (
+                        <li key={index} className="flex items-center">
+                          <FontAwesomeIcon
+                            icon={iconMap[item.icon as keyof typeof iconMap]}
+                            className="mr-2"
+                          />
+                          <span>{item.text}</span>
+                        </li>
+                      ))}
                     </ul>
                   </div>
                 </div>
 
-                <div className="w-[500px] bg-white p-4 rounded-md shadow-md mt-4">
+                {/* Photos Section */}
+                <div className="w-[500px] bg-white p-4 rounded-md shadow-md xl:mt-4">
                   <h3 className="text-lg font-bold font-segoe">Foto</h3>
                   <div className="grid grid-cols-3 gap-4 mt-4">
-                    {photos.map((photo) => (
-                      <div
-                        key={photo.id}
-                        className="flex flex-col items-center cursor-pointer"
-                        onClick={() => handleProfileClick(photo)} // Updated to pass selected photo
-                      >
-                        <img
-                          src={photo.url}
-                          alt={photo.title}
-                          className="w-full h-[150px] object-cover rounded-md"
-                        />
-                      </div>
-                    ))}
+                    {photos && photos.length > 0 ? (
+                      photos.map((photo) => (
+                        <div
+                          key={photo.id}
+                          className="flex flex-col items-center cursor-pointer"
+                          onClick={() => handleProfileClick(photo)}>
+                          <img
+                            src={photo.url}
+                            alt={photo.title}
+                            className="w-full h-[150px] object-cover rounded-md"
+                          />
+                        </div>
+                      ))
+                    ) : (
+                      <div>No photos available</div>
+                    )}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         );
-      // Handle other cases...
-      default:
-        return <div />;
     }
   };
 
@@ -176,8 +211,6 @@ const Dashboard = () => {
         <Hero activeTab={activeTab} handleTabClick={handleTabClick} />
       </div>
       <div className="w-full h-full">{renderTab()}</div>
-
-      {/* Modal for Profile Image Popup */}
       {isProfileOpen && selectedPhoto && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="relative">
@@ -187,7 +220,7 @@ const Dashboard = () => {
               X
             </button>
             <img
-              src={selectedPhoto.url}
+              src={selectedPhoto.url} // Dynamic image from the clicked photo
               alt={selectedPhoto.title}
               className="max-w-full max-h-[80vh] object-contain rounded-md"
             />
